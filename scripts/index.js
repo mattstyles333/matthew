17ab8388ccb6af48776c1c3fddfe66e6d74ffdd8
Matthew Williams <mattwilliams85@gmail.com>
2020-01-30 13:59:37 -0800

    fixes filter word wrap

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
 $(function() {
-
   var isMobile;
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-   isMobile = true;
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    isMobile = true;
 
-   // Mobile height fix
-   $('.height-fix').each(function(){
-    var h = $(this).height();
-    $(this).height(h)
-   })
+    // Mobile height fix
+    $('.height-fix').each(function() {
+      var h = $(this).height();
+      $(this).height(h);
+    });
   }
 
   // RESIZE RESETS
-  $(window).resize(function(){
+  $(window).resize(function() {
     posFilterBar($('.filter').first());
   });
 
@@ -23,14 +26,12 @@ $(function() {
     $('nav').addClass('desk');
   }
 
-
   // NAV POSITION
   var navPos = $('nav').position().top;
   var lastPos = 0;
-  var lockTimer
+  var lockTimer;
 
-  $(window).on('scroll', function () {
-
+  $(window).on('scroll', function() {
     var pos = $(window).scrollTop();
     var pos2 = pos + 50;
     var scrollBottom = pos + $(window).height();
@@ -46,65 +47,88 @@ $(function() {
     }
 
     // Link Highlighting
-    if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
-    if (pos2 > $('#about').offset().top)      { highlightLink('about'); }
-    if (pos2 > $('#portfolio').offset().top)  { highlightLink('portfolio'); }
-    if (pos2 > $('#blog').offset().top)       { highlightLink('blog'); }
-    if (pos2 > $('#contact').offset().top ||
-        pos + $(window).height() === $(document).height()) {
-          highlightLink('contact');
+    if (pos2 > $('#home').offset().top) {
+      highlightLink('home');
+    }
+    if (pos2 > $('#about').offset().top) {
+      highlightLink('about');
+    }
+    if (pos2 > $('#portfolio').offset().top) {
+      highlightLink('portfolio');
+    }
+    if (pos2 > $('#blog').offset().top) {
+      highlightLink('blog');
+    }
+    if (
+      pos2 > $('#contact').offset().top ||
+      pos + $(window).height() === $(document).height()
+    ) {
+      highlightLink('contact');
     }
 
     // Prevent Hover on Scroll
     clearTimeout(lockTimer);
-    if(!$('body').hasClass('disable-hover')) {
-      $('body').addClass('disable-hover')
+    if (!$('body').hasClass('disable-hover')) {
+      $('body').addClass('disable-hover');
     }
 
-    lockTimer = setTimeout(function(){
-      $('body').removeClass('disable-hover')
+    lockTimer = setTimeout(function() {
+      $('body').removeClass('disable-hover');
     }, 500);
   });
 
   function highlightLink(anchor) {
     $('nav .active').removeClass('active');
-    $("nav").find('[dest="' + anchor + '"]').addClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
   }
-
 
   // EVENT HANDLERS
   $('.page-link').click(function() {
-    var anchor = $(this).attr("dest");
+    var anchor = $(this).attr('dest');
     $('.link-wrap').removeClass('visible');
 
     $('nav span').removeClass('active');
-    $("nav").find('[dest="'+ anchor +'"]').addClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
 
-    $('html, body').animate({
-      scrollTop: $('#' + anchor).offset().top
-    }, 400);
+    $('html, body').animate(
+      {
+        scrollTop: $('#' + anchor).offset().top
+      },
+      400
+    );
   });
 
   $('.mdi-menu').click(function() {
     $('.link-wrap').toggleClass('visible');
   });
 
-  $('.blog-wrap').hover(  function() {
-    $('.blog-wrap').not(this).addClass('fade');
-    $( this ).addClass( "hover" );
-  }, function() {
-    $( this ).removeClass( "hover" );
-    $('.blog-wrap').removeClass('fade');
-  });
+  $('.blog-wrap').hover(
+    function() {
+      $('.blog-wrap')
+        .not(this)
+        .addClass('fade');
+      $(this).addClass('hover');
+    },
+    function() {
+      $(this).removeClass('hover');
+      $('.blog-wrap').removeClass('fade');
+    }
+  );
 
   posFilterBar($('.filter').first());
 
-  $('.filter').click(function(){
+  $('.filter').click(function() {
     posFilterBar(this);
   });
 
   function posFilterBar(elem) {
-    var origin = $(elem).parent().offset().left;
+    var origin = $(elem)
+      .parent()
+      .offset().left;
     var pos = $(elem).offset().left;
     $('.float-bar').css({
       left: pos - origin,
@@ -114,57 +138,65 @@ $(function() {
   }
 
   // GALLERY
-  $('#gallery').mixItUp({ });
+  $('#gallery').mixItUp({});
 
   function mixClear() {
-    setTimeout(function() { $('#gallery').removeClass('waypoint') }, 2000);
+    setTimeout(function() {
+      $('#gallery').removeClass('waypoint');
+    }, 2000);
   }
 
   // SCROLL ANIMATIONS
-  function onScrollInit( items, elemTrigger ) {
-    var offset = $(window).height() / 1.6
-    items.each( function() {
+  function onScrollInit(items, elemTrigger) {
+    var offset = $(window).height() / 1.6;
+    items.each(function() {
       var elem = $(this),
-          animationClass = elem.attr('data-animation'),
-          animationDelay = elem.attr('data-delay');
+        animationClass = elem.attr('data-animation'),
+        animationDelay = elem.attr('data-delay');
 
-          elem.css({
-            '-webkit-animation-delay':  animationDelay,
-            '-moz-animation-delay':     animationDelay,
-            'animation-delay':          animationDelay
-          });
+      elem.css({
+        '-webkit-animation-delay': animationDelay,
+        '-moz-animation-delay': animationDelay,
+        'animation-delay': animationDelay
+      });
 
-          var trigger = (elemTrigger) ? trigger : elem;
+      var trigger = elemTrigger ? trigger : elem;
 
-          trigger.waypoint(function() {
-            elem.addClass('animated').addClass(animationClass);
-            if (elem.get(0).id === 'gallery') mixClear(); //OPTIONAL
-            },{
-                triggerOnce: true,
-                offset: offset
-          });
+      trigger.waypoint(
+        function() {
+          elem.addClass('animated').addClass(animationClass);
+          if (elem.get(0).id === 'gallery') mixClear(); //OPTIONAL
+        },
+        {
+          triggerOnce: true,
+          offset: offset
+        }
+      );
     });
   }
 
-  setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
+  setTimeout(function() {
+    onScrollInit($('.waypoint'));
+  }, 10);
 
   // CONTACT FORM
   $('#contact-form').submit(function(e) {
     e.preventDefault();
 
-      $.ajax({
-          url: "https://formspree.io/mattwilliams85@gmail.com",
-          method: "POST",
-          data: { message: $('form').serialize() },
-          dataType: "json"
-      }).done(function(response) {
-          $('#success').addClass('expand');
-          $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-      });
+    $.ajax({
+      url: 'https://formspree.io/mattwilliams85@gmail.com',
+      method: 'POST',
+      data: { message: $('form').serialize() },
+      dataType: 'json'
+    }).done(function(response) {
+      $('#success').addClass('expand');
+      $('#contact-form')
+        .find('input[type=text], input[type=email], textarea')
+        .val('');
+    });
   });
 
   $('#close').click(function() {
     $('#success').removeClass('expand');
-  })
-
+  });
 });
