49ab88095a4e70febefffad1d2afe60215e34945
mattstyles333 <matt@radiusoffice.ie>
2020-03-13 11:55:29 +0000

    more formatting updates & added project data in

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+TO DO:
+Add image thumbnails for projects
+Add slides too
+
+
 OPTIMIZE: 
-Pause Canvas when out of view
-Loading Animation
 Optimize image sizes
 Higher quality images for portoflio (allows for 100vw on smaller sizes)
 
@@ -13,11 +16,5 @@ Browser Retest
 
 MAYBE:
 mysterious carousel bug
-Animate 'back-to-top' arrow on hover
-Add elasticity to portfolio bar
-Wipe button animation - http://tympanus.net/Development/ButtonStylesInspiration/
-Gifs for powur homepage and neversurrender preview?
-Add humor
 DL my resume Link
-Navbar border hue animation
-Make Canvas subtly interactive
+
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,168 +1,144 @@
 $(document).ready(function() {
-  // MODAL
-  var modalText = {
-    discover: {
-      title: 'ChowNow Discover',
-      tag: 'FOOD ORDERING PLATFORM.',
-      detail:
-        'ChowNow Discover is a platform that lets customers discover new local restaurants and provides business owners with tools to convert first time orders into lifelong diners.',
-      link: 'https://eat.chownow.com/'
-    },
-    ordering: {
-      title: 'ChowNow Ordering Web',
-      tag: 'WHITE-LABEL ONLINE ORDERING SOLUTION.',
-      detail:
-        'ChowNow is a commission-free online ordering system and food ordering app helping restaurants feed their hungry customers.',
-      link: 'https://direct.chownow.com/direct/195/locations/260'
-    },
-    newrelic: {
-      title: 'NewRelic.com',
-      tag: 'APPLICATION PERFORMANCE MONITORING.',
-      detail:
-        'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-      link: 'http://www.newrelic.com'
-    },
-    roambi: {
-      title: 'Roambi.com',
-      tag: 'BUSINESS ANALYTICS.',
-      detail:
-        'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-      link: 'http://www.roambi.com'
-    },
-    walker: {
-      title: 'WalkerTracker',
-      tag: 'PERFORMANCE METRICS.',
-      detail:
-        'Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.'
-    },
-    powur: {
-      title: 'Powur.com',
-      tag: 'CONSUMER POWERED MARKETING.',
-      detail:
-        'Powur is a marketing platform for lead generation, recruitment, and team building. Built with Ruby on Rails and Angular-UI. Makes use of Angular-material for front-end visuals. Features complex user tree heiarchy and commission system.',
-      link: 'http://www.powur.com/with/42'
-    },
-    mystand: {
-      title: 'MyStand',
-      tag: 'CROWD-FUNDED CHARITY.',
-      detail:
-        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.'
-    },
-    never: {
-      title: 'NeverSurrender',
-      tag: 'ALS AWARENESS.',
-      detail:
-        'NeverSurrender is a platform for the new ALS foundation mobile app in hopes to raise awareness and research funding to fight ALS. Pure JavaScript marketing site to promote the new ALS NeverSurrender app.'
-    },
-    themall: {
-      title: 'The Mall',
-      tag: 'PEER GUIDED SHOPPING.',
-      detail:
-        'The Mall is a place to follow the latest fashion purchases of your friends and favorite celebrities. Built with Node.js and Handlebars. Features the ability to import thousands of top brands products into one shopping site.'
+    // MODAL
+    var modalText = {
+        mf: {
+            title: 'Moitif',
+            tag: 'Home Furniture Webstore.',
+            detail: 'A home furniture webstore built with Xcart that specializes in modern designs and styles.',
+            link: 'https://moitif.ie/'
+        },
+        sot: {
+            title: 'Radius Office Sales Order Tracker',
+            tag: 'Internal order processing system',
+            detail: 'The SOT consolidates all of our ecommerce sales orders and purchase orders.',
+            link: 'https://sot.radiusoffice.com/'
+        },
+        ro: {
+            title: 'Radius Office',
+            tag: 'Office Supply Webstore.',
+            detail: 'Radius Office is an ecommerce store that specializes in office supplies, and it\'s built/runs on Evolution X.',
+            link: 'https://radiusoffice.ie'
+        },
+        rof: {
+            title: 'Radius Office Furniture',
+            tag: 'Office Furniture Webstore.',
+            detail: 'Radius Office Furniture is an ecommerce store that specializes in office furniture, and it\'s built/runs on Evolution X (it used to run on Wordpress).',
+            link: 'https://radiusofficefurniture.ie'
+        },
+        ofsted: {
+            title: 'Ofsted Childminder Registration',
+            tag: 'Official Childminder & Nanny registration system for the UK',
+            detail: 'An end to end, django webapp that lets you register to become a childminder, and manages the application review process.',
+            link: 'https://www.gov.uk/become-childminder-nanny/register-childminder'
+        },
+        s4l: {
+            title: 'Spex4Less',
+            tag: 'One of the largerst independent online glasses retailers',
+            detail: 'A big Magento ecommerce store that specializes in selling glasses at low prices with exceptional customer service.',
+            link: 'https://www.spex4less.com'
+        }
+    };
+
+    $('#gallery .button').on('click', function() {
+        fillModal(this.id);
+        $('.modal-wrap').addClass('visible');
+    });
+
+    $('.close').on('click', function() {
+        $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+
+    $('.mask').on('click', function() {
+        $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+
+    var carousel = $('#carousel'),
+        slideWidth = 700,
+        threshold = slideWidth / 3,
+        dragStart,
+        dragEnd;
+
+    setDimensions();
+
+    $('#next').click(function() {
+        shiftSlide(-1);
+    });
+    $('#prev').click(function() {
+        shiftSlide(1);
+    });
+
+    carousel.on('mousedown', function() {
+        if (carousel.hasClass('transition')) return;
+        dragStart = event.pageX;
+        $(this).on('mousemove', function() {
+            dragEnd = event.pageX;
+            $(this).css('transform', 'translateX(' + dragPos() + 'px)');
+        });
+        $(document).on('mouseup', function() {
+            if (dragPos() > threshold) {
+                return shiftSlide(1);
+            }
+            if (dragPos() < -threshold) {
+                return shiftSlide(-1);
+            }
+            shiftSlide(0);
+        });
+    });
+
+    function setDimensions() {
+        if (
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                navigator.userAgent
+            )
+        ) {
+            slideWidth = $(window).innerWidth();
+        }
+        $('.carousel-wrap, .slide').css('width', slideWidth);
+        $('.modal').css('max-width', slideWidth);
+        $('#carousel').css('left', slideWidth * -1);
     }
-  };
 
-  $('#gallery .button').on('click', function() {
-    fillModal(this.id);
-    $('.modal-wrap').addClass('visible');
-  });
-
-  $('.close').on('click', function() {
-    $('.modal-wrap, #modal .button').removeClass('visible');
-  });
-
-  $('.mask').on('click', function() {
-    $('.modal-wrap, #modal .button').removeClass('visible');
-  });
-
-  var carousel = $('#carousel'),
-    slideWidth = 700,
-    threshold = slideWidth / 3,
-    dragStart,
-    dragEnd;
-
-  setDimensions();
-
-  $('#next').click(function() {
-    shiftSlide(-1);
-  });
-  $('#prev').click(function() {
-    shiftSlide(1);
-  });
-
-  carousel.on('mousedown', function() {
-    if (carousel.hasClass('transition')) return;
-    dragStart = event.pageX;
-    $(this).on('mousemove', function() {
-      dragEnd = event.pageX;
-      $(this).css('transform', 'translateX(' + dragPos() + 'px)');
-    });
-    $(document).on('mouseup', function() {
-      if (dragPos() > threshold) {
-        return shiftSlide(1);
-      }
-      if (dragPos() < -threshold) {
-        return shiftSlide(-1);
-      }
-      shiftSlide(0);
-    });
-  });
-
-  function setDimensions() {
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    ) {
-      slideWidth = $(window).innerWidth();
+    function dragPos() {
+        return dragEnd - dragStart;
     }
-    $('.carousel-wrap, .slide').css('width', slideWidth);
-    $('.modal').css('max-width', slideWidth);
-    $('#carousel').css('left', slideWidth * -1);
-  }
 
-  function dragPos() {
-    return dragEnd - dragStart;
-  }
+    function shiftSlide(direction) {
+        if (carousel.hasClass('transition')) return;
+        dragEnd = dragStart;
+        $(document).off('mouseup');
+        carousel
+            .off('mousemove')
+            .addClass('transition')
+            .css('transform', 'translateX(' + direction * slideWidth + 'px)');
+        setTimeout(function() {
+            if (direction === 1) {
+                $('.slide:first').before($('.slide:last'));
+            } else if (direction === -1) {
+                $('.slide:last').after($('.slide:first'));
+            }
+            carousel.removeClass('transition');
+            carousel.css('transform', 'translateX(0px)');
+        }, 700);
+    }
 
-  function shiftSlide(direction) {
-    if (carousel.hasClass('transition')) return;
-    dragEnd = dragStart;
-    $(document).off('mouseup');
-    carousel
-      .off('mousemove')
-      .addClass('transition')
-      .css('transform', 'translateX(' + direction * slideWidth + 'px)');
-    setTimeout(function() {
-      if (direction === 1) {
-        $('.slide:first').before($('.slide:last'));
-      } else if (direction === -1) {
-        $('.slide:last').after($('.slide:first'));
-      }
-      carousel.removeClass('transition');
-      carousel.css('transform', 'translateX(0px)');
-    }, 700);
-  }
+    function fillModal(id) {
+        $('#modal .title').text(modalText[id].title);
+        $('#modal .detail').text(modalText[id].detail);
+        $('#modal .tag').text(modalText[id].tag);
+        if (modalText[id].link)
+            $('#modal .button')
+            .addClass('visible')
+            .parent()
+            .attr('href', modalText[id].link);
 
-  function fillModal(id) {
-    $('#modal .title').text(modalText[id].title);
-    $('#modal .detail').text(modalText[id].detail);
-    $('#modal .tag').text(modalText[id].tag);
-    if (modalText[id].link)
-      $('#modal .button')
-        .addClass('visible')
-        .parent()
-        .attr('href', modalText[id].link);
-
-    $.each($('#modal li'), function(index, value) {
-      $(this).text(modalText[id].bullets[index]);
-    });
-    $.each($('#modal .slide'), function(index, value) {
-      $(this).css({
-        background:
-          "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
-        backgroundSize: 'cover'
-      });
-    });
-  }
+        $.each($('#modal li'), function(index, value) {
+            $(this).text(modalText[id].bullets[index]);
+        });
+        $.each($('#modal .slide'), function(index, value) {
+            $(this).css({
+                background: "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
+                backgroundSize: 'cover'
+            });
+        });
+    }
 });
